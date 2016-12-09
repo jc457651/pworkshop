0f38783f3624d0c310d002595f2ea00a085d142d
jc457651 <harpreet9@my.jcu.edu.au>
2016-12-09 20:26:22 +1000

    Initial commit

---
+++ prac01/loops.py
@@ -0,0 +1,9 @@
+for i in range(1,21,2):
+      print(i,end=' ')
+      print()
+for k in range(0,101,10):
+      print(k,end='')
+      print()
+for j in range(20,0,-1):
+      print(j, end='')
+      print()
